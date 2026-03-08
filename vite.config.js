e21c5e346bd169de91f9e0db4bfbe89730dfc403
Ali <ali@Alis-Air.mynet>
2026-03-08 08:29:33 +0000

    fix vite config import

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'  // Root path, removes /quizzes/
-})
+  base: command === 'serve' ? '/' : '/quizzes/',
+}))
